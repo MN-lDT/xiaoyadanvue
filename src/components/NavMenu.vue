351497acbd8beeb26a5b8078c4d539d2db7ca3bd
ThundeRobot <17865617308@163.com>
2020-10-19 00:46:01 +0800

    init

---
+++ src/components/NavMenu.vue
@@ -0,0 +1,38 @@
+<template>
+  <el-row class="tac">
+    <el-col :span="24">
+      <el-menu
+        default-active="2"
+        class="el-menu-vertical-demo"
+        @open="handleOpen"
+        @close="handleClose">
+        <el-menu-item index="1">
+          <i class="el-icon-menu"></i>
+          <span slot="title">列表</span>
+        </el-menu-item>
+        <el-menu-item index="2">
+          <i class="el-icon-menu"></i>
+          <span slot="title">时间线</span>
+        </el-menu-item>
+      </el-menu>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+name: "NavMenu",
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
